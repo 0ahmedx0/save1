77d778d52af4bb24b85aa9596482283673c881fe
0ahmedx0 <144847462+0ahmedx0@users.noreply.github.com>
2025-02-08 04:00:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,12 @@
 # Note if you are trying to deploy on vps then directly fill values in ("")
 
 from os import getenv
+import os
 
-API_ID = int(getenv("API_ID", "23151406"))
-API_HASH = getenv("API_HASH", "0893a87614fae057c8efe7b85114f45a")
-BOT_TOKEN = getenv("BOT_TOKEN", "8109110306:AAH8iB0jx8b2KESo41J-Zgx6NcT7zJqWGf0")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "8118877872").split()))
-MONGO_DB = getenv("MONGO_DB", "mongodb://ahmedalsaltani30:ahmedabcd074@cluster0-shard-00-00.quu8v.mongodb.net:27017,cluster0-shard-00-01.quu8v.mongodb.net:27017,cluster0-shard-00-02.quu8v.mongodb.net:27017/?ssl=true&replicaSet=atlas-s98j2a-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0")
-LOG_GROUP = getenv("LOG_GROUP", "-1002264676623")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002264676623"))
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_ID = list(map(int, os.getenv("OWNER_ID", "").split()))
+MONGO_DB = os.getenv("MONGO_DB")
+LOG_GROUP = os.getenv("LOG_GROUP")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
