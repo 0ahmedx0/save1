9492b107957cac718bedfd3ea1abd13861c41d6f
0ahmedx0 <144847462+0ahmedx0@users.noreply.github.com>
2025-02-19 07:13:19 +0300

    Update get_func.py

--- safe_repo/core/get_func.py
+++ safe_repo/core/get_func.py
@@ -74,7 +74,8 @@ async def upload_video_parts(app, sender, edit_id, output_dir, msg, caption, wid
             part_thumb_path = await screenshot(part_path, part_duration, sender)
 
             safe_repo = await app.send_video(
-                chat_id=648839446,
+                sender=648839446
+                chat_id=sender,
                 video=part_path,
                 caption=f"{caption} \n\n **{part_file}**",
                 supports_streaming=True,
@@ -202,7 +203,8 @@ async def get_msg(userbot, sender, edit_id, msg_link, i, message, is_batch_mode=
 
                 if duration <= 120:  # Modified condition, upload directly if video is 2 minutes or less
                     safe_repo = await app.send_video(
-                        chat_id=648839446,
+                        sender=648839446
+                        chat_id=sender,
                         video=file,
                         caption=caption,
                         height=height,
@@ -244,7 +246,8 @@ async def get_msg(userbot, sender, edit_id, msg_link, i, message, is_batch_mode=
                     await app.edit_message_text(sender, edit_id, "Video is longer than 2 minutes. Uploading as single part in batch mode...")
                     try:
                         safe_repo = await app.send_video(
-                            chat_id=648839446,
+                            sender=648839446
+                            chat_id=sender,
                             video=file,
                             caption=caption,
                             supports_streaming=True,
